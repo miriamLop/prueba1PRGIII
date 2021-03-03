dd20515e317034487788ad12caa1899ac6d8b3bd
miriamLop <mlopezsurco@gmail.com>
2021-03-03 19:27:48 -0400

    Agregando proyecto inical de archivos con NIO

---
+++ archivosNIO/src/main/java/com/test/archivosnio/OperacionesArchivo.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.archivosnio;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+import java.util.Scanner;
+
+
+/**
+ * Crear un directorio, añadir un archivo al directorio y añadir contenido al
+ * archivo
+ *
+ * @author Miriam Lopez
+ */
+public class OperacionesArchivo {
+
+    public void crearDirectorio() {
+        Path path = Paths.get("E:\\programacionIII");
+        try {
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+                System.out.println("directorio creado");
+            } else {
+                System.out.println("Ya existe el directorio");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void crearArchivo() {
+        Path path = Paths.get("E:\\programacionIII\\miArchivo.txt");
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+                System.out.println("Archivo creado");
+            } else {
+                System.out.println("El archivo ya existe");
+               // agregando contenido al archivo creado anteriormente
+              //  Files.write(path, "Creando archivo con Java8".getBytes(), StandardOpenOption.APPEND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void agregarContenido(){
+        Path path = Paths.get("E:\\programacionIII\\miArchivo.txt");
+        String texto,res;
+        Scanner leer=new Scanner(System.in);
+        try {
+            System.out.println("Desea agregar contenido al archivo s/n?");
+            res=leer.nextLine();
+            while(res.equalsIgnoreCase("s")){
+                texto=leer.nextLine();
+                texto=texto+"\n";
+                Files.write(path, texto.getBytes(), StandardOpenOption.APPEND);
+                System.out.println("Desea seguir agregando contenido s/n?");
+                res=leer.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void mostrarContenido(){
+        Path path = Paths.get("E:\\programacionIII\\miArchivo.txt");
+        try {
+            List<String> lineasArchivo=Files.readAllLines(path);
+            System.out.println("----- CONTENIDO DEL ARCHIVO-----");
+            for(String linea:lineasArchivo){
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+   
+
+}
